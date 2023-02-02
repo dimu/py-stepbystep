752f292e6490efb7404c6db981f697d61698eac4
dimu <wenxiang0705@qq.com>
2023-02-02 18:17:57 +0800

    add basic syntax

--- firstlearning/basicsyntax/indentifierConvention.py
+++ firstlearning/basicsyntax/indentifierConvention.py
@@ -1,7 +1,21 @@
 # ptyhon  is a case sensitive programming language, class name start with an uppercase letter, underscore indentifier
 # is private
 
+import sys
+import getopt
+
 if 1 == 2:
     print('oh,no')
 else:
     print("yes")
+
+# get cmd line args
+print(sys.argv)
+print(len(sys.argv))
+
+
+a = b = c = 1
+c = 2
+print(a)
+print(b)
+print(c)
